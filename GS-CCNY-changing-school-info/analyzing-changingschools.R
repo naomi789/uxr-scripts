fda96b2afd5efdfc95f2b0fb5b46c961a9f712f1
Naomi Johnson <snjohnson789@gmail.com>
2023-12-04 09:17:13 -0800

    moving code around

--- GS-CCNY-changing-school-info/analyzing-changingschools.R
+++ GS-CCNY-changing-school-info/analyzing-changingschools.R
@@ -11,10 +11,16 @@ library(dplyr)
 many_bars_stacked <- function(subset_df, title_graph, name_x_axis, name_y_axis, name_for_key) {
   subset_df_long <- subset_df %>%
     gather(key = "Category", value = "Response")
-  
   # Plotting stacked bar chart
   p <- ggplot(subset_df_long, aes(x = Category, fill = Response)) +
     geom_bar(position = "stack") +
+    geom_text(
+      # aes(label = ifelse(Value == "TRUE", str_to_upper(as.character(key_col)), Value)),
+      aes(label = paste("n =", after_stat(count))),
+      stat = "count",
+      position = position_stack(vjust = 0.5), angle = 90, # adjust the vertical position of labels
+      size = 3 # adjust the size of labels
+    ) +
     labs(title = title_graph,
          x = name_x_axis,
          y = name_y_axis, 
@@ -31,6 +37,13 @@ one_stacked_bar <- function(df, select_on) {
     gather(key = "Category", value = "Response")
   p <- ggplot(df_long, aes(x = Category, fill = Response)) +
     geom_bar(position = "stack") +
+    geom_text(
+      # aes(label = Value)),
+      aes(label = paste("n =", after_stat(count))),
+      stat = "count",
+      position = position_stack(vjust = 0.5), # adjust the vertical position of labels
+      size = 3 # adjust the size of labels
+    ) +
     labs(title = select_on,
          x = "Category",
          y = "Count") +
@@ -39,10 +52,9 @@ one_stacked_bar <- function(df, select_on) {
 }
 
 show_missing_stacked_graph <- function(df, select_on, alphabetized_values) {
-  summary_table <- table(df$`The most important factor of my school search was`)
-  summary_df <- as.data.frame(table(factor(df$`The most important factor of my school search was`, levels = alphabetized_values)))
-  # print(summary_df)
-  # print(length(alphabetized_values))
+  summary_table <- table(df[[select_on]])
+  summary_df <- as.data.frame(table(factor(df[[select_on]], levels = alphabetized_values)))
+  
   # nice_colors <- viridis_pal()(20)
   p <- ggplot(summary_df, aes(x = "", y = Freq, fill = factor(str_sub(Var1, end = 20)))) +
     geom_bar(stat = "identity") +
@@ -57,20 +69,49 @@ show_missing_stacked_graph <- function(df, select_on, alphabetized_values) {
   return(p)
 }
 
-view_other_write_ins <- function(df, this_col_name, og_col_name) {
-  # drop NAs
-  short_df <- df[rowSums(is.na(df)) != ncol(df), ]
-  # print all values
-  if (nrow(short_df) > 0) {
-    # Print non-NA values
-    non_na_values <- short_df$this_col_name[!is.na(short_df$this_col_name)]
-    print(non_na_values)
-  } else {
-    print(paste('No non-NA values in the column: "', og_col_name, '"'))
+# cleaning data for AM-BD (18 columns)
+considered_v_did_not_consider <- function(subset_df) {
+  # find count of folks who responded "did not consider" for any given question
+  count_df <- data.frame(column_name = character(0), "NA - Did not consider this" = integer(0), "Other Values" = integer(0), stringsAsFactors = FALSE)
+  
+  # Loop through each column in the original data frame
+  for (column_to_analyze in colnames(subset_df)) {
+    # Count occurrences
+    counts <- table(subset_df[[column_to_analyze]])
+    
+    # Extract counts for "NA - Did not consider this" and other values
+    na_count <- counts["NA - Did not consider this"]
+    other_count <- sum(counts[setdiff(names(counts), "NA - Did not consider this")])
+    
+    # Append the results to the count_df
+    count_df <- rbind(count_df, c(column_name = column_to_analyze, "NA - Did not consider this" = na_count, "Other Values" = other_count))
   }
+  # now graph considered/not considered
+  colnames(count_df) <- c("Aspect of K-12 School", "Did not consider", "Other")
+  count_df_long <- tidyr::gather(count_df, key = "Response", value = "Count", -"Aspect of K-12 School")
+  count_df_long$Count <- as.numeric(count_df_long$Count)
+  count_df_long$`Aspect of K-12 School` <- factor(count_df_long$`Aspect of K-12 School`, levels = unique(count_df_long$`Aspect of K-12 School`))
+  count_df_long$label <- substr(count_df_long$`Aspect of K-12 School`, 1, 65)
+  
+  p <- ggplot(count_df_long, aes(x = `Aspect of K-12 School`, y = Count, fill = Response)) +
+    geom_bar(stat = "identity") +
+    geom_text(aes(label = ifelse(Response == "Did not consider", Count, "")),
+              position = position_stack(vjust = 0.5), color = "white") +  # Add labels of "Count" for "Did not consider"
+    geom_text(aes(label = ifelse(Response == "Other", Count, "")),
+              position = position_stack(vjust = 0.95), color = "white") +  # Add labels for "Other"
+    geom_text(aes(label = ifelse(Response == "Other", label, "Considered")),
+              position = position_stack(vjust = 0.02), color = "black", size = 3, angle = 90, hjust = 0) +
+    
+    
+    labs(title = "Stacked Bar Chart",
+         x = "Aspect of K-12 School",
+         y = "Count") +
+    theme_minimal() +
+    theme(axis.text.x = element_blank())  # Remove x-axis labels
+  return(p)
 }
 
-# COMPLETED FUNCTIONS
+
 # simple histogram
 plot_histo <- function(df, title_graph, x_vals) {
   p <- ggplot(df, aes(x = !!rlang::sym(x_vals))) +
@@ -83,6 +124,7 @@ plot_histo <- function(df, title_graph, x_vals) {
   return(p)
 }
 
+
 # convert multi-col data to TRUE/FALSE and then visualize
 plot_stacked <- function(df, key_col, count_responses, title_graph, preferred_order) {
   df <- tidyr::gather(df, key = key_col, value = "Value")
@@ -90,6 +132,13 @@ plot_stacked <- function(df, key_col, count_responses, title_graph, preferred_or
   
   p <- ggplot(df, aes(x = key_col, fill = factor(Value))) +
     geom_bar(stat = "count") +
+    geom_text(
+      aes(label = ifelse(Value == "TRUE", str_to_upper(as.character(key_col)), Value)),
+      # aes(label = paste("n =", after_stat(count))),
+      stat = "count",
+      position = position_stack(vjust = 0.5), angle = 90, # adjust the vertical position of labels
+      size = 3 # adjust the size of labels
+    ) +
     labs(title = paste("Some parents (n =", count_responses, ") ", title_graph),
          x = key_col,
          y = "Count") +
@@ -100,9 +149,7 @@ plot_stacked <- function(df, key_col, count_responses, title_graph, preferred_or
 }
 
 
-
 # GET DATA FROM SURVEYMONKEY
-# clean_df <- read.csv("School-Choice-For-CCNY-2023-12-03-10pm.csv", header=T, na.strings=c("","NA"))
 clean_df <- read_excel(path = "School-Choice-For-CCNY-2023-12-03-10pm.xlsx")
 
 # CLEANING DATA
@@ -188,14 +235,18 @@ grade_level_df$non_na_count <- rowSums(!is.na(grade_level_df))
 grade_level_df$elem <- rowSums(!is.na(df[, 24:30])) > 0
 grade_level_df$mid <- rowSums(!is.na(df[, 31:33])) > 0
 grade_level_df$high <- rowSums(!is.na(df[, 34:37])) > 0
-sum_reported_kids_grade <- sum(rowSums(grade_level_df[, c("elem", "mid", "high")]) > 0)
+# set up function call
 elem_mid_high_df <- grade_level_df %>% select("elem", "mid", "high")
-preferred_order <- c("elem", "mid", "high")
 key_col <- "Grade_Level"
-# df <- elem_mid_high_df
-# count_responses <- sum_reported_kids_grade
+sum_reported_kids_grade <- sum(rowSums(grade_level_df[, c("elem", "mid", "high")]) > 0)
+title_graph <- "reported kids' grade during search"
+preferred_order <- c("elem", "mid", "high")
+# df <- grade_level_df %>% select("elem", "mid", "high")
+# key_col <- "Grade_Level"
+# count_responses <- sum(rowSums(grade_level_df[, c("elem", "mid", "high")]) > 0)
 # title_graph <- "reported kids' grade during search"
-plot_stacked(elem_mid_high_df, key_col, sum_reported_kids_grade, "reported kids' grade during search", preferred_order)
+# preferred_order <- c("elem", "mid", "high")
+plot_stacked(elem_mid_high_df, key_col, sum_reported_kids_grade, title_graph, preferred_order)
 
 # MOST IMPORTANT FACTOR OF MY SEARCH; AL aka
 # "The most important factor of my school search was"
@@ -203,8 +254,7 @@ important_search_factor <- df[,38]
 important_search_factor <- important_search_factor[rowSums(is.na(important_search_factor)) != ncol(important_search_factor), ]
 select_on = "The most important factor of my school search was"
 title_graph = "Most important factor of my school search was:"
-plot_histo(important_search_factor, title_graph, select_on)
-# TO DO WORKING HERE NOW
+# plot_histo(important_search_factor, title_graph, select_on)
 alphabetized_values <- sort(unlist(df_sub_questions[, 39:56, drop = FALSE][1,]))
 show_missing_stacked_graph(important_search_factor, select_on, alphabetized_values)
 
@@ -250,69 +300,22 @@ plot_stacked(race_df, key_col, count_responses, title_graph, preferred_order)
 # TODO: visualize
 
 
-# VISUALIZATION
+# DIFFICULTY FINDING INFO ON FACTORS
 # one giant set of stacked bar graphs for AM-BD (18 columns)
-# just get relevant columns
 subset_df <- df[, 39:56, drop = FALSE]
-# Rename columns to first row; remove that row
 colnames(subset_df) <- df_sub_questions[, 39:56, drop = FALSE]
-# drop folks who didn't answer any of these questions
 subset_df <- subset_df[rowSums(is.na(subset_df)) != ncol(subset_df), ]
-# Check if there are any NA values in subset_df & print
-# has_na <- any(is.na(subset_df))
-# print(has_na)
 title_graph = "Ease/difficulty finding info on aspects of K-12 school"
 name_y_axis = "Count"
 name_for_key = "Difficulty finding info"
 name_x_axis = "Aspects of K-12 school"
 many_bars_stacked(subset_df, title_graph, name_x_axis, name_y_axis, name_for_key)
+
 # making custom graph for each column
 for (col in colnames(subset_df)) {
   print(one_stacked_bar(subset_df, col))
 }
-# find count of folks who reponded "did not consider" for any given question
-count_df <- data.frame(column_name = character(0), "NA - Did not consider this" = integer(0), "Other Values" = integer(0), stringsAsFactors = FALSE)
 
-# Loop through each column in the original data frame
-for (column_to_analyze in colnames(subset_df)) {
-  # Count occurrences
-  counts <- table(subset_df[[column_to_analyze]])
-  
-  # Extract counts for "NA - Did not consider this" and other values
-  na_count <- counts["NA - Did not consider this"]
-  other_count <- sum(counts[setdiff(names(counts), "NA - Did not consider this")])
-  
-  # Append the results to the count_df
-  count_df <- rbind(count_df, c(column_name = column_to_analyze, "NA - Did not consider this" = na_count, "Other Values" = other_count))
-}
-# now graph considered/not considered
-colnames(count_df) <- c("Aspect of K-12 School", "Did not consider", "Other")
-count_df_long <- tidyr::gather(count_df, key = "Response", value = "Count", -"Aspect of K-12 School")
-count_df_long$Count <- as.numeric(count_df_long$Count)
-count_df_long$`Aspect of K-12 School` <- factor(count_df_long$`Aspect of K-12 School`, levels = unique(count_df_long$`Aspect of K-12 School`))
-count_df_long$label <- substr(count_df_long$`Aspect of K-12 School`, 1, 65)
+# visualize considered v. did not consider
+considered_v_did_not_consider(subset_df)
 
-p <- ggplot(count_df_long, aes(x = `Aspect of K-12 School`, y = Count, fill = Response)) +
-  geom_bar(stat = "identity") +
-  geom_text(aes(label = ifelse(Response == "Did not consider", Count, "")),
-            position = position_stack(vjust = 0.5), color = "white") +  # Add labels of "Count" for "Did not consider"
-  geom_text(aes(label = ifelse(Response == "Other", Count, "")),
-            position = position_stack(vjust = 0.95), color = "white") +  # Add labels for "Other"
-  geom_text(aes(label = ifelse(Response == "Other", label, "")),
-            position = position_stack(vjust = 0.02), color = "black", size = 3, angle = 90, hjust = 0) +
-  
-  
-  labs(title = "Stacked Bar Chart",
-       x = "Aspect of K-12 School",
-       y = "Count") +
-  theme_minimal() +
-  theme(axis.text.x = element_blank())  # Remove x-axis labels
-print(p)
-
-# shorten titles of columns
-# remove second row??
-# pie chart of column L: "During the past 12 months, how seriously have you considered enrolling your child/children in a different K-12 school in the US?"
-
-# HYPOTHESIS
-# parents who have already picked (v. not-picked-school parents) feel that various data/info is harder to find
-# 
